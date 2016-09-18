6da85a836de7c876bf3339783ba0508dfcb0b5f0
Nagaraju Somisetty <Nagaraju Somisetty>
2016-09-18 14:48:42 +0530

    Gopi is entered into project.

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main{
    public static void main(String[] args){
     
       System.out.println("Welcome Mr. Nagaraju");
+      Systen.out.println("Welcome to  Mr.Gopi Krish");
     }
 }
